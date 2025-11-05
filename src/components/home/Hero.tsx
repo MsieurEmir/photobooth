3455f0becd3441dab2f2215f54422e236e940deb
MsieurEmir <maildeemir@gmail.com>
2025-11-05 21:51:01 +0100

    Updated Hero.tsx

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -55,16 +55,13 @@ const Hero = () => {
       </div>
 
       {/* Scroll Indicator */}
-      <motion.div
-        className="absolute bottom-28 bottom-12 sm:bottom-8 md:bottom-8 left-1/2 transform -translate-x-1/2 z-20"
-        animate={{ y: [0, 10, 0] }}
-        transition={{ repeat: Infinity, duration: 1.5 }}
-      ><motion.div
+     <motion.div
   className="absolute bottom-28 sm:bottom-16 md:bottom-12 left-1/2 transform -translate-x-1/2 z-20"
   animate={{ y: [0, 10, 0] }}
   transition={{ repeat: Infinity, duration: 1.5 }}
 >
 
+        
         <a
           href="#features"
           className="text-white flex flex-col items-center"
